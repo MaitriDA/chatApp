eb47f7b11a9e0c894075d278d95fe3f7bbbc0814
Ruchika Wadhwa <ruchika01.rw@gmail.com>
2021-04-05 14:42:54 +0530

    Todo List w/o css

--- src/layout/Components/PersonalSpace.js
+++ src/layout/Components/PersonalSpace.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import fire from '../../helper/db';
 import './PersonalSpace.css';
+import Todo from "../todoList/Todo";
 
 const PersonalSpace=(props)=>{
     return(
        <div className="mainPersonalSpace">
-       PersonalSpace
+       <Todo/>
        </div>
     );
 
